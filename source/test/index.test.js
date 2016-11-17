83bb71c1710c3feeeda7f95ad27e700400377e74
rogerbf <roger@bergfalk.nu>
2016-11-17 22:20:40 +0100

    reaching all branches

--- source/test/index.test.js
+++ source/test/index.test.js
@@ -5,3 +5,20 @@ test(`events`, assert => {
   assert.ok(events, `exports something`)
   assert.end()
 })
+
+test(`events()`, assert => {
+  const app = events()
+  assert.equal(typeof (app.on), `function`)
+  assert.equal(typeof (app.once), `function`)
+  assert.equal(typeof (app.emit), `function`)
+  assert.end()
+})
+
+test(`events(app)`, assert => {
+  const app = {}
+  events(app)
+  assert.equal(typeof (app.on), `function`)
+  assert.equal(typeof (app.once), `function`)
+  assert.equal(typeof (app.emit), `function`)
+  assert.end()
+})
--- source/test/once.test.js
+++ source/test/once.test.js
@@ -12,7 +12,19 @@ test(`adds subscriber to subscriptions object`, assert => {
   const subscribers = {}
   const events = once(subscribers)
   const fn = data => noop()
-  events.once(`message`, fn)
+  const unsubscribe = events.once(`message`, fn)
   assert.deepEqual(subscribers, { message: [fn] })
+  unsubscribe()
+  assert.deepEqual(subscribers, { message: [] })
+  assert.end()
+})
+
+test(`subscriber to subscriptions object (multiple subscribers)`, assert => {
+  const fnA = () => noop()
+  const subscribers = { primary: [fnA] }
+  const events = once(subscribers)
+  const fnB = data => noop()
+  events.once(`primary`, fnB)
+  assert.deepEqual(subscribers, { primary: [fnA, fnB] })
   assert.end()
 })
