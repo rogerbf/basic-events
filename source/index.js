38a190737bc7f956b1ace339c82c76f8a1f3f43a
rogerbf <roger@bergfalk.nu>
2016-11-19 21:50:09 +0100

    using new state manager

--- source/index.js
+++ source/index.js
@@ -1,9 +1,11 @@
+import initState from './library/initState'
 import on from './library/on'
 import once from './library/once'
 import emit from './library/emit'
 
 export default (target = {}) => {
-  const subscriptions = {}
+  const subscriptions = initState()
+
   return Object.assign(
     target,
     on(subscriptions),
